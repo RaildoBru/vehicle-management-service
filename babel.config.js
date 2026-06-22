21205be146bc22139431914cf8084c7c0ad09200
RaildoBru <raildobruno@gmail.com>
2026-06-22 15:28:00 -0300

    Corrigindo babel config

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,5 @@ export default {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript'
-  ],
-  ignore: [
-    'node_modules',
-    '**/node_modules/**',
-    'src/generated/client' // caso você use um output customizado no schema.prisma
   ]
 };
