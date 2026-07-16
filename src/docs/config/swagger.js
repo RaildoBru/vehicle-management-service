ecb4f7dbd36e5ad530e3b38215a4339d03920d97
RaildoBru <raildobruno@gmail.com>
2026-07-15 21:44:21 -0300

    Ajuste na doc

--- src/docs/config/swagger.js
+++ src/docs/config/swagger.js
@@ -4,7 +4,7 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Vehicle Sales API',
+      title: 'Vehicle Management API',
       version: '1.0.0',
       description: 'API para gerenciamento de veículos'
     },
